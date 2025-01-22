ec8d7ef3c67066f3c9202875fe31ebfbbbb62a73
Isaac Leon <Isaacmlp714@gmail.com>
2025-01-22 16:17:45 +0100

    Dashboard Cliente

--- src/main/java/Controller/DashboardClientesController.java
+++ src/main/java/Controller/DashboardClientesController.java
@@ -1,4 +1,33 @@
 package Controller;
 
+import javafx.scene.input.MouseEvent;
+import javafx.fxml.FXML;
+
 public class DashboardClientesController {
+
+    @FXML
+    void AgregarCliente(MouseEvent event) {
+
+    }
+
+    @FXML
+    void BuscarCliente(MouseEvent event) {
+
+    }
+
+    @FXML
+    void EliminarCliente(MouseEvent event) {
+
+    }
+
+    @FXML
+    void ModificarCliente(MouseEvent event) {
+
+    }
+
+    @FXML
+    void VerCliente(MouseEvent event) {
+
+    }
+
 }
--- src/main/java/Controller/DashboardController.java
+++ src/main/java/Controller/DashboardController.java
@@ -2,33 +2,32 @@ package Controller;
 
 import Utils.OpenView;
 import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
 
 public class DashboardController {
     OpenView Open = new OpenView();
 
     @FXML
-    void Inventario(MouseEvent event) throws Exception {
+    void Inventario() throws Exception {
         Open.DashboardInventario();
     }
 
     @FXML
-    void Clientes(MouseEvent event) throws Exception {
+    void Clientes() throws Exception {
         Open.DashboardClientes();
     }
 
     @FXML
-    void Facturar(MouseEvent event) throws Exception {
+    void Facturar() throws Exception {
         Open.DashboardFacturar();
     }
 
     @FXML
-    void Facturas(MouseEvent event) throws Exception {
+    void Facturas() throws Exception {
         Open.DashboardFacturas();
     }
 
     @FXML
-    void Monedas(MouseEvent event) throws Exception {
+    void Monedas() throws Exception {
         Open.DashboardMonedas();
     }
 }
