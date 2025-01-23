cfd8c63c319a95fdb2f0df1e8da0d1bc9575e56a
Isaac Leon <Isaacmlp714@gmail.com>
2025-01-23 12:04:23 +0100

    Agregar Clientes

--- src/main/java/Controller/Cliente/AgregarClienteController.java
+++ src/main/java/Controller/Cliente/AgregarClienteController.java
@@ -1,4 +1,73 @@
 package Controller.Cliente;
 
+import Model.Cliente.AgregarClienteModel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
 public class AgregarClienteController {
+    AgregarClienteModel model = new AgregarClienteModel();
+
+    @FXML
+    private TextField ApellidoTXT;
+
+    @FXML
+    private TextField CedulaTXT;
+
+    @FXML
+    private TextField CorreoTXT;
+
+    @FXML
+    private TextField DireccionTXT;
+
+
+    @FXML
+    private TextField NombreTXT;
+
+    @FXML
+    private TextField TelefonoTXT;
+
+    private boolean IsEmpty () {
+        return NombreTXT.getText().isEmpty() || ApellidoTXT.getText().isEmpty() || CedulaTXT.getText().isEmpty() || TelefonoTXT.getText().isEmpty() || DireccionTXT.getText().isEmpty() || CorreoTXT.getText().isEmpty();
+    }
+
+    private void CleanFields() {
+        NombreTXT.setText("");
+        ApellidoTXT.setText("");
+        CedulaTXT.setText("");
+        TelefonoTXT.setText("");
+        DireccionTXT.setText("");
+        CorreoTXT.setText("");
+    }
+
+    @FXML
+    void BtnGuardar(ActionEvent event) {
+        if (IsEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error!");
+            alert.setHeaderText(null);
+            alert.setContentText("Debe llenar todos los campos");
+            alert.showAndWait();
+            return;
+        }
+        GuardarCliente();
+    }
+
+    private void GuardarCliente() {
+        if (model.GuardarCliente(NombreTXT.getText(), ApellidoTXT.getText(), CedulaTXT.getText(), TelefonoTXT.getText(), DireccionTXT.getText(), CorreoTXT.getText())) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Cliente Agregado!");
+            alert.setHeaderText(null);
+            alert.setContentText("El cliente ha sido agregado");
+            alert.showAndWait();
+            CleanFields();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error!");
+            alert.setHeaderText(null);
+            alert.setContentText("Error al agregar cliente");
+            alert.showAndWait();
+        }
+    }
 }
--- src/main/java/Model/Cliente/AgregarClienteModel.java
+++ src/main/java/Model/Cliente/AgregarClienteModel.java
@@ -1,4 +1,38 @@
 package Model.Cliente;
 
+import Utils.ConectBD;
+import javafx.scene.control.Alert;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class AgregarClienteModel {
+    ConectBD Conect = new ConectBD();
+
+    public boolean GuardarCliente(String nombre,String apellido, String cedula, String telefono, String direccion,  String correo) {
+        String sql = "INSERT INTO Clientes.Cliente VALUES (?,?,?,?,?,?)";
+        try (Connection conexion = Conect.Conect();
+             PreparedStatement ps = conexion.prepareStatement(sql)) {
+
+            ps.setString(1, nombre);
+            ps.setString(2, apellido);
+            ps.setString(3, cedula);
+            ps.setString(4, telefono);
+            ps.setString(5, direccion);
+            ps.setString(6, correo);
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error!");
+            alert.setHeaderText(null);
+            alert.setContentText(ex.getMessage());
+            alert.showAndWait();
+            return false;
+        }
+    }
+
 }
